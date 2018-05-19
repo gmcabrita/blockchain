879a8778da727b28597c19ea0560bfcdbaafd012
Gonçalo Cabrita <_@gmcabrita.com>
2018-05-19 18:47:00 +0100

    main: add a basic proof of work algorithm

--- main.go
+++ main.go
@@ -3,47 +3,126 @@ package main
 import (
 	"bytes"
 	"crypto/sha256"
+	"encoding/binary"
 	"fmt"
-	"strconv"
+	"log"
+	"math"
+	"math/big"
 	"time"
 )
 
-// Block defines a block in the chain
+// Block mining difficulty
+const targetBits = 24 // TODO: implement target adjusting algorithm
+const maxNonce = math.MaxInt64
+
+// ProofOfWork represents a proof of work
+type ProofOfWork struct {
+	block  *Block
+	target *big.Int
+}
+
+// NewProofOfWork creates a new ProofOfWork
+func NewProofOfWork(b *Block) *ProofOfWork {
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256-targetBits))
+
+	pow := &ProofOfWork{b, target}
+
+	return pow
+}
+
+// IntToHex converts a 64 bit integer to a byte array
+func IntToHex(num int64) []byte {
+	buff := new(bytes.Buffer)
+	err := binary.Write(buff, binary.BigEndian, num)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return buff.Bytes()
+}
+
+func (pow *ProofOfWork) prepareData(nonce int) []byte {
+	return bytes.Join(
+		[][]byte{
+			pow.block.PrevBlockHash,
+			pow.block.Data,
+			IntToHex(pow.block.Timestamp),
+			IntToHex(int64(targetBits)),
+			IntToHex(int64(nonce)),
+		},
+		[]byte{},
+	)
+}
+
+// Run performs a proof of work
+func (pow *ProofOfWork) Run() (int, []byte) {
+	var hashInt big.Int
+	var hash [32]byte
+	nonce := 0
+
+	fmt.Printf("Mining the block containing \"%s\"\n", pow.block.Data)
+	for nonce < maxNonce {
+		data := pow.prepareData(nonce)
+		hash = sha256.Sum256(data)
+		fmt.Printf("\r%x", hash)
+		hashInt.SetBytes(hash[:])
+
+		if hashInt.Cmp(pow.target) == -1 {
+			break
+		} else {
+			nonce++
+		}
+	}
+
+	fmt.Printf("\n\n")
+
+	return nonce, hash[:]
+}
+
+// Validate validates the Block's proof of work
+func (pow *ProofOfWork) Validate() bool {
+	var hashInt big.Int
+
+	data := pow.prepareData(pow.block.Nonce)
+	hash := sha256.Sum256(data)
+	hashInt.SetBytes(hash[:])
+
+	return hashInt.Cmp(pow.target) == -1
+}
+
+// Block represents a block in the chain
 type Block struct {
 	Timestamp     int64
 	Data          []byte
 	PrevBlockHash []byte
 	Hash          []byte
+	Nonce         int
 }
 
-// NewBlock creates a new Block, given some `data` and a `prevBlockHash`
+// NewBlock creates a new Block
 func NewBlock(data string, prevBlockHash []byte) *Block {
-	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
-	block.SetHash()
+	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}, 0}
+	pow := NewProofOfWork(block)
+	nonce, hash := pow.Run()
+
+	block.Hash = hash[:]
+	block.Nonce = nonce
 
 	return block
 }
 
-// SetHash sets the hash of the block based on its `Timestamp`, `Data`, and `PrevBlockHash``
-func (b *Block) SetHash() {
-	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
-	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
-	hash := sha256.Sum256(headers)
-
-	b.Hash = hash[:]
-}
-
-// Blockchain defines a chain of `Block` structs
+// Blockchain represents a chain of Block structs
 type Blockchain struct {
 	blocks []*Block
 }
 
-// NewBlockchain creates a new Blockchain, inserting into it its genesis block
+// NewBlockchain creates a new Blockchain, inserting a genesis block into it
 func NewBlockchain() *Blockchain {
 	return &Blockchain{[]*Block{NewBlock("init", []byte{})}}
 }
 
-// AddBlock adds a new block to the Blockchain, given some `data`
+// AddBlock adds a new block to the Blockchain
 func (bc *Blockchain) AddBlock(data string) {
 	prevBlock := bc.blocks[len(bc.blocks)-1]
 	newBlock := NewBlock(data, prevBlock.Hash)
@@ -60,6 +139,8 @@ func main() {
 		fmt.Printf("Previous hash: %x\n", block.PrevBlockHash)
 		fmt.Printf("Data: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
+		pow := NewProofOfWork(block)
+		fmt.Printf("Valid: %v\n", pow.Validate())
 		fmt.Println()
 	}
 }
